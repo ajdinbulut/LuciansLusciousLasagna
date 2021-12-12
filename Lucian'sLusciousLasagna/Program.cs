782f519ddceb9301c2d99dbfa4aaa57609ec3583
buli-dot <72310468+buli-dot@users.noreply.github.com>
2021-12-12 13:54:03 +0100

    Add project files.

---
+++ Lucian'sLusciousLasagna/Program.cs
@@ -0,0 +1,23 @@
+﻿class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int a)
+    {
+        return 40 - a;
+    }
+    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return layers * 2;
+    }
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int a, int b)
+    {
+        return (a * 2) + b;
+    }
+}
